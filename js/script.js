74775feef08173685884bc10f546da57b9f4c68a
DevKola <koladeayinde@gmail.com>
2023-11-29 16:11:12 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+let dropdown = document.getElementsByClassName("dropdown-btn");
+let dropdownAngle = document.querySelector(".angle");
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    let dropdownContent = this.nextElementSibling;
+
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+const tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+menu.addEventListener("click", (e) => {
+  sidenav.style.marginLeft = "0";
+});
